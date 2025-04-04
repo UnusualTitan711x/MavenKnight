2098af0f850c6b2934d16c92b5a1b7429cceb1ac
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-04-04 22:53:04 +0100

    fixed some bugs

--- scripts/podium.gd
+++ scripts/podium.gd
@@ -4,14 +4,12 @@ class_name Podium
 @onready var collision_shape: CollisionShape3D = $Area3D/CollisionShape3D
 @onready var cylinder: MeshInstance3D = $CSGBakedMeshInstance3D/MeshInstance3D
 
-@export var next_level: PackedScene
+@export var next_level_path: String
 
 var enemies: Array
 var initial_enemy_count: int
 var enemies_clear: bool = false
 var manager: GameManager
-
-
 
 func activate():
 	collision_shape.disabled = false
@@ -19,8 +17,10 @@ func activate():
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Player:
-		# 
-		if next_level:
-			get_tree().change_scene_to_packed(next_level)
+		if next_level_path:
+			var next = load(next_level_path)
+			
+			if next:
+				get_tree().change_scene_to_packed(next)
 		else:
 			get_tree().reload_current_scene()
